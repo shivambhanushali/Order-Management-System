56ac13b784df98df9d0c60769e188321cb436c7c
shivambhanushali <130748142+shivambhanushali@users.noreply.github.com>
2024-07-18 22:06:54 +0530

    route done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,34 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { SaladComponent } from './salad/salad.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { BurgerComponent } from './burger/burger.component';
+import { PastaComponent } from './pasta/pasta.component';
+import { DessertComponent } from './dessert/dessert.component';
 
 export const routes: Routes = [
-  {}
+   {
+    path:'add-menu',
+    component:MenuComponent
+   },
+   {
+      path:'add-salad',
+      component:SaladComponent
+   },
+   {
+      path:'add-pasta',
+      component:PastaComponent
+   },
+   {
+      path:'add-dessert',
+      component:DessertComponent
+   },
+   {
+      path:'add-burger',
+      component:BurgerComponent
+   },
+   {
+      path:'add-pizza',
+      component:PizzaComponent
+   },
 ];
